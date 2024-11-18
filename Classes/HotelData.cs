04c0cbb6001e145c1b923c764126bece708c850a
davidperjans <perjansdavid@gmail.com>
2024-11-18 09:58:20 +0100

    Fixat funktion för att checka ut gäst, samt adderat fakturor. När du checkar ut skapas det en faktura som visas under fliken "Visa fakturor"

---
+++ Classes/HotelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Classes
+{
+    public class HotelData
+    {
+        public List<Room> Rooms { get; set; } = new List<Room>();
+        public List<Invoice> Invoices { get; set; } = new List<Invoice>();
+    }
+}
